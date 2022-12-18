53f1dcb770fdb55b9c80c6ffaa1e4d49c9746aee
Wenderson Wanzeller <wenderson@wanzeller.com.br>
2022-12-18 17:08:09 +0000

    Mudança layout

--- includes/head.php
+++ includes/head.php
@@ -6,9 +6,9 @@
         <title>Bootstrap demo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
-        <body class="bg-dark text-light">
+        <body>
             <div class="container">
-                <div class="shadow p-3 mb-5 mt-3 bg-primary rounded">
+                <div class="text-light shadow p-3 mb-5 mt-3 bg-primary rounded">
                     <h1>Desenvolvimento WEB</h1>
                     <p>Exemplo de CRUD com PHP orientado a objetios</p>
                 </div>
